e4f98268c63bbc329e8ffebffe8a0a25f9d9a240
margOnline <margourey@gmail.com>
2026-05-17 10:21:38 +0100

    Better looking spinner

--- src/components/AppSpinner.vue
+++ src/components/AppSpinner.vue
@@ -1,40 +1,37 @@
 <script setup></script>
 
 <template>
-  <div class="spinner"></div>
+  <div role="status" class="spinner">
+    <span class="sr-only">Loading...</span>
+  </div>
 </template>
 
 <style scoped>
 .spinner {
-  display: grid;
-  place-items: center;
-  position: relative;
-  width: 150px;
-  height: 150px;
-  filter: var(--accent-brightness-filter);
-  background-image: radial-gradient(circle at center, var(--accent-color) 40%, transparent 50%);
+  width: 3rem;
+  height: 3rem;
+  border: 3px solid var(--grey-400);
   border-radius: 50%;
-  &:after, &:before {
-    content: "";
-    position: absolute;
-    border: 8px solid var(--accent-color);
-    border-radius: 50%;
-  }
-  &:after {
-    width: 150px;
-    height: 150px;
-    border-color: var(--accent-color) transparent var(--accent-color) transparent;
-    animation: dual-ring 1s linear infinite;
-  }
-  &:before {
-    width: 120px;
-    height: 120px;
-    border-color: transparent var(--accent-color) transparent var(--accent-color);
-    animation: dual-ring-reverse 0.8s linear infinite;
-  }
+  display: inline-block;
+  position: relative;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+.spinner::after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  border: 3px solid;
+  border-color: var(--grey-250) transparent;
 }
 
-@keyframes dual-ring {
+@keyframes rotation {
   0% {
     transform: rotate(0deg);
   }
@@ -43,12 +40,12 @@
   }
 }
 
-@keyframes dual-ring-reverse {
+@keyframes spin {
   0% {
-    transform: rotate(360deg);
+    transform: rotate(0deg);
   }
   100% {
-    transform: rotate(0deg);
+    transform: rotate(360deg);
   }
 }
 </style>
