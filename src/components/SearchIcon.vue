b534c690a1ee8d940c4d4013f88acea26118b85b
margOnline <margourey@gmail.com>
2026-05-18 16:40:11 +0100

    Component for search icon

--- src/components/SearchIcon.vue
+++ src/components/SearchIcon.vue
@@ -4,8 +4,8 @@
     xmlns="http://www.w3.org/2000/svg"
     x="0px"
     y="0px"
-    width="100"
-    height="100"
+    width="25"
+    height="25"
     viewBox="0 0 50 50"
   >
     <path
@@ -13,3 +13,9 @@
     ></path>
   </svg>
 </template>
+
+<style scoped>
+svg {
+  fill: var(--grey-250);
+}
+</style>
