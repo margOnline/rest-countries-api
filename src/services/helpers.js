c9bd812c3b019f65a952965baf9314bf662f05d6
margOnline <margourey@gmail.com>
2026-05-18 16:36:51 +0100

    Reusable debounce function to help with search for a country feature

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -1,3 +1,5 @@
+import { ref, onUnmounted } from 'vue';
+
 export const constants = {
   allLabel: '--All--',
   filterLabel: 'Filter by region',
@@ -5,7 +7,31 @@ export const constants = {
 export const formatNumber = (number) => {
   return Intl.NumberFormat().format(number)
 }
-
 export const capitalise = (word) => {
   word.charAt(0).toUpperCase() + word.slice(1)
+}
+
+export function useDebounce(fxn, delay) {
+// Define a function that takes 2 arguments:
+// 1. a function that can accept any number of arguments and returns a value of any type
+// 2. the delay in ms before executing the function
+  const timeoutID = ref(null);
+
+  const debouncedFn = (...args) => {
+    if (timeoutID.value !== null) {
+      clearTimeout(timeoutID.value);
+    }
+
+    timeoutID.value = window.setTimeout(() => {
+      fxn(...args);
+    }, delay);
+  };
+
+  onUnmounted(() => {
+    if (timeoutID.value !== null) {
+      clearTimeout(timeoutID.value);
+    }
+  });
+
+  return debouncedFn;
 }
