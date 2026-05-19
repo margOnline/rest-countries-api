ae03947ddcb3d04eecb5e76ded2b5e2a59dcd3b6
margOnline <margourey@gmail.com>
2026-05-19 21:34:02 +0100

    Update styleing of search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,51 @@
 <script setup>
 import { ref } from 'vue'
-const searchLocation = ref('')
+import SearchIcon from './SearchIcon.vue'
+const search = ref('')
 </script>
 
 <template>
-  <form class="search-form">
-    <input type="text" placeholder="Search for a country..." v-model="searchLocation" />
-    <button @click="findCountries" type="submit">Search</button>
-    {{ searchLocation }}
+  <form @submit.prevent class="custom-shadow" autocomplete="off">
+    <button>
+      <SearchIcon />
+    </button>
+    <input
+      v-model="search"
+      type="text"
+      placeholder="Search for a country..."
+      @keyup="handleSearch"
+    />
   </form>
 </template>
 
-<style scoped></style>
+<style scoped>
+form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 500px;
+  background-color: var(--white);
+  color: var(--grey-400);
+  border-radius: 0.25rem;
+  border: none;
+  overflow: hidden;
+}
+input {
+  width: 100%;
+  height: 100%;
+  border: none;
+  font-size: 1rem;
+}
+input::placeholder {
+  color: var(--grey-350);
+}
+input:focus {
+  outline: none;
+}
+button {
+  border: none;
+}
+svg {
+  padding-block-start: 0.25rem;
+}
+</style>
